7ecb66f385d04a8a1434ddde81140d4d2a363898
LeptonComm <shadowpostfish@gmail.com>
2026-05-01 11:21:27 +0300

    patch
    Префикс верфи и номер шаттла теперь сохраняются при переименовании: скопирована функция StationRename со сплитом строки; Заменена в RenameShuttleSystem;
    Конкретизированы правила: Прописаны методы урегулирования различных видов нарушений; Поведение игрока с имплантом защиты разума

--- Content.Server/Maps/NameGenerators/NanotrasenNameGenerator.cs
+++ Content.Server/Maps/NameGenerators/NanotrasenNameGenerator.cs
@@ -10,6 +10,7 @@ public sealed partial class NanotrasenNameGenerator : StationNameGenerator
     ///     Where the map comes from. Should be a two or three letter code, for example "VG" for Packedstation.
     /// </summary>
     [DataField("prefixCreator")] public string PrefixCreator = default!;
+    [DataField("shipyardPrefix")] public string SuffixCode = default!;
 
     //private string Prefix => "NT";
     private string Prefix => "";
@@ -18,8 +19,8 @@ public sealed partial class NanotrasenNameGenerator : StationNameGenerator
     public override string FormatName(string input)
     {
         var random = IoCManager.Resolve<IRobustRandom>();
-
+        var ShuttleNumber = random.Next(100,1000); // Lua: Выбор случайного числа от 100 до 999 включительно в качестве номера
         //return string.Format(input, $"{Prefix}{PrefixCreator}", $"{random.Pick(SuffixCodes)}-{random.Next(0, 1000):D3}");
-        return string.Format(input, $"{Prefix}{PrefixCreator}", $"{random.Next(0, 10000):D4}");
+        return string.Format("{0} {1}-{2}", input, SuffixCode, ShuttleNumber); // Lua: сборка названия шаттла из: названия, префикса верфи, сгенерированного номера
     }
 }
